30c4261ddc26e8236cae680f1f398eb8df330dcd
Christopher Dow <chris.dow8@gmail.com>
2014-11-28 09:22:21 -0800

    Converted debugger actor to a FSM actor

--- src/main/scala/com/github/cdow/actor/debugger/DebuggerActor.scala
+++ src/main/scala/com/github/cdow/actor/debugger/DebuggerActor.scala
@@ -2,13 +2,14 @@ package com.github.cdow.actor.debugger
 
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, Props, ActorRef}
+import akka.actor.{FSM, Props, ActorRef}
 import akka.io.Tcp._
 import akka.io.{Tcp, IO}
 import akka.util.ByteString
 
 sealed trait DebuggerState
 case object Initial extends DebuggerState
+case object Bound extends DebuggerState
 case object Connected extends DebuggerState
 case object Running extends DebuggerState
 
@@ -16,32 +17,52 @@ object DebuggerActor {
 	def props(port: Int, listener: ActorRef) = Props(new DebuggerActor(port, listener))
 }
 
-class DebuggerActor(port: Int, listener: ActorRef) extends Actor {
+class DebuggerActor(port: Int, listener: ActorRef) extends FSM[DebuggerState, Option[ActorRef]] {
 	import context.system
+
+	val HANDSHAKE = ByteString.fromString("JDWP-Handshake", "US-ASCII")
 
 	IO(Tcp) ! Bind(self, new InetSocketAddress(port))
 
-	def receive: Actor.Receive = {
-		case b @ Bound(localAddress) =>
-		// do some logging or setup ...
+	startWith(Initial, None)
 
-		case CommandFailed(_: Bind) => context stop self
+	when(Initial) {
+		case Event(b @ Bound(localAddress)) =>
+			goto(Bound)
+		case Event(CommandFailed(_: Bind)) =>
+			context stop self
+			stay
+	}
 
-		case c @ Connected(remote, local) =>
+	when(Bound) {
+		case Event(c @ Connected(remote, local)) =>
 			listener ! c
 			val connection = sender()
 			connection ! Register(self)
-			context become {
-				case data: ByteString =>
-					connection ! Write(data)
-				case Received(data) =>
-					listener ! data
-				case "close" =>
-					connection ! Close
-				case connectionClosed: ConnectionClosed =>
-					context unbecome()
-					listener ! connectionClosed
-			}
+			goto(Connected) using Some(connection)
 	}
+
+	when(Connected) {
+		case Event(Received(data), connection: Option[ActorRef]) if data == HANDSHAKE =>
+			connection.foreach(_ ! Write(HANDSHAKE))
+			goto(Running)
+		case Event(connectionClosed: ConnectionClosed) =>
+			listener ! connectionClosed
+			goto(Bound) using None
+	}
+
+	when(Running) {
+		case Event(data: ByteString, Some(connection)) =>
+			connection ! Write(data)
+			stay
+		case Event(Received(data)) =>
+			listener ! data
+			stay
+		case Event(connectionClosed: ConnectionClosed) =>
+			listener ! connectionClosed
+			goto(Bound) using None
+	}
+
+	initialize()
 }
 
